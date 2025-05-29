8b3c62204fea5e4d7f998b456920942599ab157c
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2025-05-30 02:53:52 +0800

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -3,7 +3,7 @@ import { R2Explorer } from "r2-explorer";
 // Read all available configurations here: https://r2explorer.com/getting-started/configuration/
 export default R2Explorer({
 	readonly: false,
-  cfAccessTeamName: '1trust',
+  cfAccessTeamName: "1trust"
 	// basicAuth: {
 	//   username: 'username',
 	//   password: 'password'
